638f02b99eed5c263f5eefd9158435f56cc63226
Airosmaa Maiju A <airosmaa@da5-cs-bk107-39.helsinki.fi>
2016-03-18 13:12:33 +0200

    html

--- config/routes.php
+++ config/routes.php
@@ -1,9 +1,19 @@
 <?php
 
-  $routes->get('/', function() {
-    HelloWorldController::index();
-  });
+$routes->get('/', function() {
+HelloWorldController::index();
+});
 
-  $routes->get('/hiekkalaatikko', function() {
-    HelloWorldController::sandbox();
-  });
+$routes->get('/hiekkalaatikko', function() {
+HelloWorldController::sandbox();
+});
+$routes->get('/game', function() {
+HelloWorldController::etusivu();
+});
+$routes->get('/game/1', function() {
+HelloWorldController::muokkaa();
+});
+
+$routes->get('/login', function() {
+HelloWorldController::login();
+});
