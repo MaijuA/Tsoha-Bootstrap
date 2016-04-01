e7ccb12ded8e2d8b075a86c2e00c2107836f2b98
Airosmaa Maiju A <airosmaa@da2-ex-dk107-11.helsinki.fi>
2016-04-01 11:51:32 +0300

    painikkeet

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -6,7 +6,8 @@ class HelloWorldController extends BaseController {
 
 public static function index() {
 // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-echo 'Tämä on etusivu!';
+View::make('suunnitelmat/kirjautuminen.html');
+//echo 'Tämä on etusivu!';
 }
 
 public static function sandbox() {
