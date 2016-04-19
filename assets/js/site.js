9962ceb5688231f007378c1d6f26f2c395045b00
Airosmaa Maiju A <airosmaa@da2-ex-dk107-08.helsinki.fi>
2016-04-19 14:10:15 +0300

    destroy

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,3 +1,12 @@
-$(document).ready(function(){
-  //alert('Hello World!');
+$(document).ready(function () {
+    //alert('Hello World!');
+    $('form.destroy-form').on('submit', function (submit) {
+        // Otetaan kohdelomakkeesta data-confirm-attribuutin arvo
+        var confirm_message = $(this).attr('data-confirm');
+        // Pyydetään käyttäjältä vahvistusta
+        if (!confirm(confirm_message)) {
+            // Jos käyttäjä ei anna vahvistusta, ei lähetetä lomaketta
+            submit.preventDefault();
+        }
+    });
 });
